a86134f419cb001bd4e9e92543398838e5b732c4
Hemanth-Moka <2300030439@kluniversity.in>
2025-10-15 03:44:22 +0530

    v1

---
+++ Backend/src/main/java/com/klef/dev/DockerBackendApplication.java
@@ -0,0 +1,14 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBackendApplication.class, args);
+		System.out.println("Backend is Running ...");
+	}
+
+}
